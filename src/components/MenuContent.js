9f86251f236c1d0c2d3b459101e291c2181f209f
github username <cory.baker@gramercytech.com>
2019-02-02 20:47:36 -0500

    added more projects

--- src/components/MenuContent.js
+++ src/components/MenuContent.js
@@ -1,20 +1,23 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
-import triangle from "../assets/triangle.png"
-import mePic from "../assets/mebest.png"
-import mamVid from "../assets/mamVid.mp4"
-import astellas from "../assets/astellas.mov"
-import top19 from "../assets/top19.png"
+import triangle from "../assets/triangle.png";
+import mePic from "../assets/mebest.png";
+import mamVid from "../assets/mamVid.mp4";
+import wkol from "../assets/wkol.MP4";
+import nacs from "../assets/nacs-tobacco-kiosk.mov";
 
-import App from "../App"
+import astellas from "../assets/astellas.mov";
+import top19 from "../assets/top19.png";
+
+import App from "../App";
 class MenuContent extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       class: "fadeIn",
       information: "",
       opacity: {}
-    }
+    };
   }
 
   handleAbout = () => {
@@ -22,7 +25,7 @@ class MenuContent extends Component {
       this.setState({
         class: "fadeOut",
         opacity: { opacity: 0 }
-      })
+      });
     } else {
     }
     this.setState({
@@ -31,36 +34,39 @@ class MenuContent extends Component {
       about: (
         <div className="textContain">
           <p id="menuP1" className="fadeIn">
-            I’m a full stack nerd with an itch for dissecting confusing concepts
-            and a knack for writing functional, organized, and DRY code. I
-            currently work as a web and app devloper at Gramercy Tech, a digital
-            consultancy.<br />
-            <br />Outside of tech, I love spending time with my dog (Sasha),
-            playing video games too much, and being outdoors.
+            I’m a full stack and mobile developer with an itch for dissecting
+            confusing concepts and a knack for writing functional, organized,
+            and DRY code. I currently work as a web and app devloper at Gramercy
+            Tech, a digital consultancy.
+            <br />
+            <br />
+            Outside of tech, I love spending time with my dog (Sasha), playing
+            video games too much, and being outdoors.
           </p>
         </div>
       )
-    })
-  }
+    });
+  };
 
   handleProjects = () => {
     if (this.state.class === "fadeIn") {
       this.setState({
         class: "fadeOut",
         opacity: { opacity: 0 }
-      })
+      });
     } else {
     }
     this.setState({
       contact: "",
       about: "",
       projects: (
-        <div className="textContain">
+        <div className="textContain fadeIn">
           <h5 id="menuP2" className="fadeIn">
             Master-A-Million
           </h5>
           <p>React Native Mobile App</p>
           <video className="fadeIn" id="mamVid" controls src={mamVid} />
+          <br />
           <br />
           <br />
           <h5 id="menuP2" className="fadeIn">
@@ -70,35 +76,52 @@ class MenuContent extends Component {
           <video className="fadeIn" id="mamVid" controls src={astellas} />
           <br />
           <br />
+          <br />
+          <h5 id="menuP2" className="fadeIn">
+            WKOL-AstraZeneca
+          </h5>
+          <p>React Native Mobile App</p>
+          <video className="fadeIn" id="mamVid" controls src={wkol} />
+          <br />
+          <br />
+          <br />
+          <h5 id="menuP2" className="fadeIn">
+            Reynolds Tobacco Product Showcase
+          </h5>
+          <p>React Web App</p>
+          <video className="fadeIn" id="mamVid" controls src={nacs} />
+          <br />
+          <br />
+          <br />
           <h5 id="menuP2" className="fadeIn">
             Top19
           </h5>
-
           <p id="menuP4">Top News Storis</p>
           <a target="_blank" href="https://s3.amazonaws.com/top19/index.html">
             <img id="top19" src={top19} />
           </a>
           <br />
           <br />
+          <br />
           <p className="fadeIn">and many more...</p>
         </div>
       )
-    })
-  }
+    });
+  };
 
   handleContact = () => {
     if (this.state.class === "fadeIn") {
       this.setState({
         class: "fadeOut",
         opacity: { opacity: 0 }
-      })
+      });
     } else {
     }
     this.setState({
       about: "",
       projects: "",
       contact: (
-        <div className="textContain">
+        <div className="textContain fadeIn">
           <a id="menuP3">douglasb2008@gmail.com</a>
           <br />
           <br />
@@ -126,8 +149,8 @@ class MenuContent extends Component {
           </a>
         </div>
       )
-    })
-  }
+    });
+  };
 
   render() {
     return (
@@ -155,7 +178,7 @@ class MenuContent extends Component {
           {this.state.contact}
         </div>
       </div>
-    )
+    );
   }
 }
-export default MenuContent
+export default MenuContent;
